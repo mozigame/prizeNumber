007130697701f9d4fc450773306040d314437c74
zhongjie <zhongjie@17shihui.com>
2018-03-20 14:42:38 +0800

    gdhpv-pc蛋蛋

--- game/gdhpv/work.py
+++ game/gdhpv/work.py
@@ -23,7 +23,7 @@ def handler(job_name='',prizeItem=[]):
     _current_item4api={}
     for item in _list4api:
         #print(item[2])
-        if('bj_pk10'==item[4] or 'xg_lhc'==item[4] or 'luckship'==item[4] or 'bj_ssc'==item[4] or 'tw_ssc'==item[4]):
+        if('bj_pk10'==item[4] or 'xg_lhc'==item[4] or 'luckship'==item[4] or 'bj_ssc'==item[4] or 'tw_ssc'==item[4] or 'pc_dd'==item[4]):
             _current_item4api[item[4]]=item[2]
         else:
             _current_item4api[item[4]]='20'+str(item[2])
@@ -63,13 +63,13 @@ def handler(job_name='',prizeItem=[]):
                         #print(j['opentimestamp'])
                         numbers=''
                         item =PrizeNumberItem()
-                        if('bj_pk10'==url[0] or 'xg_lhc'==url[0] or 'luckship'==url[0] or 'bj_ssc'==url[0] or 'tw_ssc'==url[0]):
+                        if('bj_pk10'==url[0] or 'xg_lhc'==url[0] or 'luckship'==url[0] or 'bj_ssc'==url[0] or 'tw_ssc'==url[0] or 'pc_dd'==url[0]):
                             item.issue=j['numeroNo'].replace('-','')
                         else:
                             item.issue=j['numeroNo'].replace('-','')[2:]
                         logger.info("new issue :"+item.issue)
                         
-                        if ('ah_k3'==url[0] or 'sh_k3'==url[0]  or 'js_k3'==url[0] or 'cq_ssc'==url[0] or 'tj_ssc'==url[0] or 'xj_ssc'==url[0] or 'bj_ssc'==url[0] or 'tw_ssc'==url[0]):
+                        if ('ah_k3'==url[0] or 'sh_k3'==url[0]  or 'js_k3'==url[0] or 'cq_ssc'==url[0] or 'tj_ssc'==url[0] or 'xj_ssc'==url[0] or 'bj_ssc'==url[0] or 'tw_ssc'==url[0] or 'pc_dd'==url[0]):
                             
                             for t in j['winNo']:
                                 numbers=numbers+','+t
